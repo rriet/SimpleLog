368af15c1219f16f13aa881201e4f990aec2d083
rriet <32526307+rriet@users.noreply.github.com>
2024-12-19 13:45:05 -0500

    Initial Commit

---
+++ SimpleLog/SimpleLogApp.swift
@@ -0,0 +1,32 @@
+//
+//  SimpleLogApp.swift
+//  SimpleLog
+//
+//  Created by Ricardo Brito Riet Correa on 12/19/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SimpleLogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
