8c338b3bbdbb42c61f879ef2cdf87d06a71d6485
rriet <32526307+rriet@users.noreply.github.com>
2025-01-10 18:53:55 -0500

    First Commit

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,18 @@
+//
+//  Item+CoreDataClass.swift
+//  SimpleLog
+//
+//  Created by Ricardo Brito Riet Correa on 1/10/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+    let longDate = {
+        return 
+    }
+}
--- SimpleLog/Persistence.swift
+++ SimpleLog/Persistence.swift
@@ -14,7 +14,7 @@ struct PersistenceController {
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
+        for _ in 0..<5 {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
         }
